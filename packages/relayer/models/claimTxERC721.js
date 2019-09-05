0b9881e7feb5ab60586ade9da9c301ee6e8720a3
Amir Jumaniyazov <amiromayer@gmail.com>
2019-09-05 16:11:56 +0500

    Added fix for models

--- packages/relayer/models/claimTxERC721.js
+++ packages/relayer/models/claimTxERC721.js
@@ -4,9 +4,8 @@ const claimTxERC721Schema = new mongoose.Schema({
   weiAmount: { type: String, required: true },
   nftAddress: { type: String, required: true },
   tokenId: { type: Number, required: true },
-  expiration: { type: Number, required: true },
+  expirationTime: { type: Number, required: true },
   linkId: { type: String, required: true, unique: true },
-  linkdropMasterAddress: { type: String, required: true },
   receiverAddress: { type: String, required: true },
   senderAddress: { type: String, required: true },
   txHash: { type: String, required: true, unique: true }
