3708f8452a52434b454e810d3e154cb4a62f23aa
spacehaz <spacehaz@gmail.com>
2019-10-24 14:45:28 +0300

    added fix

--- packages/apps/gnosis-linkdrop-dapp/data/store/saga/user/every/enable-module.js
+++ packages/apps/gnosis-linkdrop-dapp/data/store/saga/user/every/enable-module.js
@@ -2,16 +2,17 @@ import { put, select } from 'redux-saga/effects'
 import { initializeSdk } from 'data/sdk'
 import { defineNetworkName } from '@linkdrop/commons'
 import config from 'app.config.js'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import Web3 from 'web3'
+console.log({ Web3 })
 
 const generator = function * ({ payload }) {
   try {
     const sdk = yield select(generator.selectors.sdk)
     const safe = yield select(generator.selectors.safe)
-    console.log({ safe })
-    const enableData = yield sdk.getEnableLinkdropModuleData(safe)
-
-    console.log({ enableData })
-
+    const data = yield sdk.getEnableLinkdropModuleData(safe)
+    // const txHash = yield web3.eth.sendTransaction({ to: sdk.createAndAddModules, data }, { value: 0 })
+    // console.log({ txHash })
     // yield put({ type: 'USER.SET_CHAIN_ID', payload: { chainId } })
     // yield put({ type: 'USER.SET_SDK', payload: { sdk } })
   } catch (e) {
